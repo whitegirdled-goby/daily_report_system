8b1a975d7b124107952dc8255780cb7146c19212
machumn <machumn@hotmail.com>
2021-08-09 10:31:20 +0900

    トップページアクションの作成

--- src/main/java/actions/TopAction.java
+++ src/main/java/actions/TopAction.java
@@ -1,23 +1,47 @@
 package actions;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
+import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
 
 public class TopAction extends ActionBase {
 
+    private ReportService service;
+
     @Override
     public void process() throws ServletException, IOException {
+        service = new ReportService();
         invoke();
+        service.close();
     }
 
     /*
      * 一覧画面を表示
      */
     public void index() throws ServletException, IOException {
+
+        // セッションからログイン中の従業員情報を取得
+        EmployeeView loginEmployee = (EmployeeView)getSessionScope(AttributeConst.LOGIN_EMP);
+
+        // ログイン中の従業員が作成した日報データを1ページ分取得する
+        int page = getPage();
+        List<ReportView> reports = service.getMinePerPage(loginEmployee, page);
+
+        // ログイン中の従業員が作成した日報の件数を取得
+        long myReportCount = service.countAllMine(loginEmployee);
+
+        putRequestScope(AttributeConst.REPORTS, reports);
+        putRequestScope(AttributeConst.REP_COUNT, myReportCount);
+        putRequestScope(AttributeConst.PAGE, page);
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
 
         // セッションにフラッシュメッセージが設定されている場合はリクエストスコープに保持
         String flush = getSessionScope(AttributeConst.FLUSH);
